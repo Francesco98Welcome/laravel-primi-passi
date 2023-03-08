8bfe40e093dc485dcd71c21a1ebd4aab738e0ffb
Francesco98Welcome <frankbenvenuti04@gmail.com>
2023-03-08 16:52:34 +0100

    First es. Laravel √

---
+++ resources/views/home.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>First project Laravel</title>
+    <style>
+        body {
+            background-color: black;
+        }
+
+        h1 {
+            color: rgba(0, 0, 0, 0.5);
+            font-family: Verdana, Geneva, Tahoma, sans-serif;
+            padding: 20px;
+            width: 70%;
+            margin: 0 auto;
+            background-color: whitesmoke;
+        }
+    </style>
+</head>
+
+<body>
+    <h1>Hello Laravel, è stato un patimento installarti &#8987</h1>
+</body>
+
+</html>
